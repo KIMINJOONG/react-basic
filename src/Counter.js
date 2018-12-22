c84e7959cb03546c4a0466a2be2c374453e52dc7
KIMINJOONG <ijboym216@gmail.com>
2018-12-22 18:53:32 +0900

    컴포넌트가 사용될때 상황에 따라 호출되는 lifeCycleAPI

--- src/Counter.js
+++ src/Counter.js
@@ -5,6 +5,33 @@ class Counter extends Component {
         number: 0
     }
 
+    constructor(props) {
+        super(props);
+        console.log('constructor');
+    }
+    componentWillMount() {
+        console.log('componentWillMount (deprecated)');
+    }
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+    
+    shouldComponentUpdate(nextProps, nextState){
+        console.log(nextProps.number + ", "+ nextState.number);
+        console.log('shouldComponentUpdate');
+        if(nextState.number % 5 === 0) return false;
+        return true;
+    }
+
+    UNSAFE_componentWillUpdate() {
+        console.log('componentWillUpdate');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log(prevProps.number + ", " + prevState.number);
+        console.log('componentDidUpdate');
+    }
+
     handleIncrease = () => {
         const { number } = this.state;
         this.setState({
