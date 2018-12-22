69bea5f861b735e8571807c0e9e08647807dba8f
KIMINJOONG <ijboym216@gmail.com>
2018-12-22 21:56:35 +0900

    검색기능 추가 리액트 이때까지의 내용 요약

--- src/PhoneInfoList.js
+++ src/PhoneInfoList.js
@@ -7,7 +7,11 @@ class PhoneInfoList extends Component {
         , onRemove: () => console.warn('onRemove not defined')
         , onUpdate: () => console.warn('onUpdate not defined')
     }
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.data !== this.props.data;
+    }
     render() {
+        console.log('render PhoneInfoList');
         const { data, onRemove, onUpdate } = this.props;
         const list = data.map(
             info => (
