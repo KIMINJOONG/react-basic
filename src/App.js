53f7d20abac6f39cad5b1cfbbadaba6496e0c212
KIMINJOONG <ijboym216@gmail.com>
2018-12-22 13:33:54 +0900

    defaultProps와 props, state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import MyName from './MyName';
 
 class App extends Component {
+
   render() {
-    const style = {
-      backgroundColor: 'black'
-      , padding: '16px'
-      , color: 'white'
-      , fontSize: '12px'
-    };
+    
     return (
-      <div className="App">
-        {/*주석은 이렇게*/ }
-        <h1 // 태그사이에
-        >리액트</h1>
-      </div>      
+      <MyName name="와우" />     
     );
   }
 }
