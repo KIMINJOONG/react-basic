a13aa58fc9e0dc2f52b4d4a48acb37378d881240
KIMINJOONG <ijboym216@gmail.com>
2018-12-22 12:26:01 +0900

    화살표 함수

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div>
         {
-          (function() {
+          (() => {
             if(value === 1) return <div>1이다!</div>
             if(value === 2) return <div>2다!</div>
             if(value === 3) return <div>3이다!</div>
