5ff2a17df3a0b8fcfea584262c73875bd2956704
KIMINJOONG <ijboym216@gmail.com>
2018-12-22 12:24:46 +0900

    조건이 여러개일경우 굳이 JSX안에서 하고싶을때

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     const name = 'injoong!';
+    const value = 1;
     return (
       <div>
         {
-          name === 'injoong!' && <div>인중이다!</div>
+          (function() {
+            if(value === 1) return <div>1이다!</div>
+            if(value === 2) return <div>2다!</div>
+            if(value === 3) return <div>3이다!</div>
+            return <div>없다</div>
+          })()
         }
       </div>
       
