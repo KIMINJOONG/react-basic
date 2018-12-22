17faed531ac5c55331816d183ec11a47067160ca
KIMINJOONG <ijboym216@gmail.com>
2018-12-22 20:13:24 +0900

    배열을 다루는 방법

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import './App.css';
 import MyName from './MyName';
 import Counter from './Counter';
 import PracticeSetState from './PracticeSetState';
+import { stringify } from 'querystring';
 import PhoneForm from './PhoneForm';
+import PhoneInfoList from './PhoneInfoList';
 
 class App extends Component {
+  id = 2
+  state = {
+    information : [
+      {
+        id:0
+        , name: '김인중'
+        , phone: '010-0000-0000'
+
+      }
+      , {
+        id: 1
+        , name: '홍길동'
+        , phone: '010-0000-0001'
+      }
+    ]
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data})
+    })
   }
 
   render() {
+    const {information} = this.state;
     
     return (
       <div>
@@ -20,6 +42,8 @@ class App extends Component {
         <PhoneForm 
           onCreate={this.handleCreate}
           />
+          {JSON.stringify(information)}
+          <PhoneInfoList data={this.state.information} />
       </div> 
     );
   }
